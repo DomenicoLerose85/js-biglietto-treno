11da43cded71686bc2163fb7c8741e4a8c8ab023
DomenicoLerose85 <domenicolerose85@libero.it>
2022-03-24 15:17:15 +0100

    excercise_started

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert ('ciao')
+// alert ('ciao')
+
+var kiloMetri = prompt('Quanti kilometri vorrai percorrere ?');
+var userAge = prompt('Quanti anni hai ?');
+
+let x = 20
+let y = 20
+
+
+
+console.log (kiloMetri, userAge)
+
+document.getElementById(prezzobiglietto);
