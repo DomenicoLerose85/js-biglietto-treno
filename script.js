324ec7bebdaf821482a617c44112d6228f41915a
DomenicoLerose85 <domenicolerose85@libero.it>
2022-03-24 15:37:25 +0100

    got_stuck

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 var kiloMetri = prompt('Quanti kilometri vorrai percorrere ?');
 var userAge = prompt('Quanti anni hai ?');
 
-let x = 20
-let y = 20
+let x = 20;
+let y = 0.21;
 
+z = x * y;
 
+if (userAge < 18) {
+    discount = 20%;
+} else if (userAge > 65); {
+    discount = 40%;
+} 
 
-console.log (kiloMetri, userAge)
+console.log (kiloMetri, userAge, z);
 
-document.getElementById(prezzobiglietto);
+document.getElementById('prezzobiglietto'). innerHTML = z;
